c6d83331087c2ba6a1b4c5b84a0becb714b21a9f
Ezra-531 <112780637+Ezra-531@users.noreply.github.com>
2024-09-14 09:30:15 -0700

    Update payload.mjs

--- payload.mjs
+++ payload.mjs
@@ -266,6 +266,12 @@
             document.querySelector('#extdbg').onclick = function () {
                 dbgext(false);
             }
+            document.querySelectorAll('.hardcoded').forEach(el => {el.onclick = function () {
+                let extid = el.getAttribute("ext");
+                console.log(el.innerText, extid);
+                dbgext(false, extid);
+                }
+            });
             document.querySelector('#cleanup').onclick = function () {
                 dbgext(true);
             }
